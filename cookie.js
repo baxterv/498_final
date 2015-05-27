47bb5667bfb14c3d5fd23f03f92622f8f6df7634
baxterv <baxterv000@gmail.com>
2015-05-27 15:19:27 -0700

    idk what I did

--- cookie.js
+++ cookie.js
@@ -6,9 +6,13 @@ var dataset;
 
 // copied code
 
-d3.csv("cookie.csv", type, function(error, data) {
-  x.domain(data.map(function(d) { return d.item; }));
-  y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+d3.csv("cookie.csv", function(error, cookie) {
+  if(error) return console.warn(error);
+  fastfood.forEach(function(d) {
+  });
+  dataset = cookie;
+  drawvis(cookie);
+});
 
 var margin = {top: 20, right: 20, bottom: 30, left: 40},
     width = 960 - margin.left - margin.right,
@@ -18,6 +22,7 @@ var x = d3.scale.ordinal()
     .rangeRoundBands([0, width], .1);
 
 var y = d3.scale.linear()
+    .domain([0, 100])
     .range([height, 0]);
 
 var xAxis = d3.svg.axis()
@@ -27,17 +32,13 @@ var xAxis = d3.svg.axis()
 var yAxis = d3.svg.axis()
     .scale(y)
     .orient("left")
-    .ticks(10, "%");
+    //.ticks(10, "%");
 
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-d3.tsv("data.tsv", type, function(error, data) {
-  x.domain(data.map(function(d) { return d.letter; }));
-  y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
 
   svg.append("g")
       .attr("class", "x axis")
@@ -53,7 +54,7 @@ d3.tsv("data.tsv", type, function(error, data) {
       .attr("dy", ".71em")
       .style("text-anchor", "end")
       .text("Frequency");
-
+function drawVis(dataset) {
   svg.selectAll(".bar")
       .data(data)
     .enter().append("rect")
@@ -63,7 +64,17 @@ d3.tsv("data.tsv", type, function(error, data) {
       .attr("y", function(d) { return y(d.frequency); })
       .attr("height", function(d) { return height - y(d.frequency); });
 
-});
+}
+  svg.selectAll(".bar")
+      .data(data)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.letter); })
+      .attr("width", x.rangeBand())
+      .attr("y", function(d) { return y(d.frequency); })
+      .attr("height", function(d) { return height - y(d.frequency); });
+
+//});
 
 function type(d) {
   d.frequency = +d.frequency;
